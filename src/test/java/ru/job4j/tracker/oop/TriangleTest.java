d5949f46f16fc083017b41da74720f374d8bad46
PK rabota <klimenko161@gmail.com>
2021-10-04 12:54:26 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642 #211788]

--- src/test/java/ru/job4j/tracker/oop/TriangleTest.java
+++ src/test/java/ru/job4j/tracker/oop/TriangleTest.java
@@ -25,4 +25,14 @@ public class TriangleTest {
         double rsl = triangle.area();
         assertThat(rsl, closeTo(1.5, 0.001));
     }
+
+    @Test
+    public void impossibleTriangle() {
+        Point a = new Point(5, 0);
+        Point b = new Point(6, 0);
+        Point c = new Point(8, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(-1.0, 0.001));
+    }
 }
