2feb07c75a8e4621fbe03d60c80ae79aa9397608
shKlimenko <klimenko161@gmail.com>
2021-10-05 23:43:11 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item frmStart = new Item();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String currentDateTimeFormat = frmStart.getCreated().format(formatter);
+        System.out.println(currentDateTimeFormat);
+    }
+}
