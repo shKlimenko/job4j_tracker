1c588c45962d8ebc45a2d7f2419b9aebfa98c1dc
PK rabota <klimenko161@gmail.com>
2021-10-09 11:49:27 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -53,4 +53,13 @@ public class Tracker {
         }
         return false;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        items[index] = null;
+        System.arraycopy(items, index + 1, items, index, size - index - 1);
+        items[size - 1] = null;
+        size--;
+        return true;
+    }
 }
