0ccf1014ece8411481b70086ced2f907067388d2
PK rabota <klimenko161@gmail.com>
2021-10-18 11:17:18 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #217436]

--- src/main/java/ru/job4j/tracker/ex/Fact.java
+++ src/main/java/ru/job4j/tracker/ex/Fact.java
@@ -2,12 +2,12 @@ package ru.job4j.tracker.ex;
 
 public class Fact {
     public static void main(String[] args) {
-        new Fact().calc(0);
+        new Fact().calc(-2);
     }
 
     public static int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("Argument should be positive");
+            throw new IllegalArgumentException("The argument must be greater than or equal to 0");
         }
         int rsl = 1;
         for (int index = 1; index <= n; index++) {
