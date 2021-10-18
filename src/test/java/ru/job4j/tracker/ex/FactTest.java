b9552674ce259e3cbde45425a3757b5fc795c4e5
PK rabota <klimenko161@gmail.com>
2021-10-18 10:56:02 +0300

    0.3. Тестирование исключений с junit [#219366]

--- src/test/java/ru/job4j/tracker/ex/FactTest.java
+++ src/test/java/ru/job4j/tracker/ex/FactTest.java
@@ -2,8 +2,6 @@ package ru.job4j.tracker.ex;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class FactTest {
     @Test(expected = IllegalArgumentException.class)
     public void whenStartGreaterTThenFinish() {
