1f14496b353062b514e0c6a81783e28d556b552a
Master <klimenko161@gmail.com>
2021-10-25 23:50:08 +0300

    1. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,9 +1,21 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class SingleTracker {
+    static private SingleTracker instance = null;
+
     private Tracker tracker = new Tracker();
 
-    /* реализовать singleton */
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
 
     public Item add(Item item) {
         return tracker.add(item);
@@ -13,5 +25,19 @@ public class SingleTracker {
         return tracker.findById(id);
     }
 
-    /* Остальные методы из класса Tracker. */
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
 }
