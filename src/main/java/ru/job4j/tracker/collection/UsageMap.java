b9e84ec90c36b8c35e57c642d6418d663f447f30
Master <klimenko161@gmail.com>
2021-11-15 23:44:48 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/tracker/collection/UsageMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("ivanov@yandex.ru", "Ivan Ivanov");
+        map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
+        map.remove("ivanov@yandex.ru");
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " " + value);
+        }
+    }
+}
