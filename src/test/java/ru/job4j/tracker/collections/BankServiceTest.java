8e36a50604da15f0b8e4b5495d51d2f63bccfef1
PK rabota <klimenko161@gmail.com>
2021-11-18 11:21:39 +0300

    update

--- src/test/java/ru/job4j/tracker/collections/BankServiceTest.java
+++ src/test/java/ru/job4j/tracker/collections/BankServiceTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class BankServiceTest {
-
     @Test
     public void addUser() {
         User user = new User("3434", "Petr Arsentev");
@@ -22,25 +21,5 @@ public class BankServiceTest {
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
         assertNull(bank.findByRequisite("34", "5546"));
-    }
-
-    @Test
-    public void addAccount() {
-        User user = new User("3434", "Petr Arsentev");
-        BankService bank = new BankService();
-        bank.addUser(user);
-        bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertThat(bank.findByRequisite("3434", "5546").getBalance(), is(150D));
-    }
-
-    @Test
-    public void transferMoney() {
-        User user = new User("3434", "Petr Arsentev");
-        BankService bank = new BankService();
-        bank.addUser(user);
-        bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        bank.addAccount(user.getPassport(), new Account("113", 50D));
-        bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
-        assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));
     }
 }
