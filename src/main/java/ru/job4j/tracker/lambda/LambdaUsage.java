b2271bed428af4d250aa9fa2ba0fccc4c4d45b2a
PK rabota <klimenko161@gmail.com>
2021-11-29 10:47:06 +0300

    2.2. Лямбда блок [#249208]

--- src/main/java/ru/job4j/tracker/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/tracker/lambda/LambdaUsage.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 public class LambdaUsage {
     public static void main(String[] args) {
         Comparator<String> cmpDescSize = (left, right) -> {
-            System.out.println("Compare - " + right.length() + " : " + left.length());
-            return right.length() - left.length();
+            System.out.println("Compare - " + left.length() + " : " + right.length());
+            return Integer.compare(left.length(), right.length());
         };
     }
 }
