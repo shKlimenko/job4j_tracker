6710ac7331b1ef253619975982b9793ecf8ecb52
Master <klimenko161@gmail.com>
2021-12-02 23:06:36 +0300

    2. Список адресов [#110062]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+    @Test
+    public void whenCollectAdress() {
+        Address adr1 = new Address("Shakhty", "Lenina", 220, 2);
+        Address adr2 = new Address("Rostov-on-Don", "Cheremukhina", 17, 7);
+        Address adr3 = new Address("Volgograd", "Malogvardeiskaya", 999, 666);
+
+        Profile prof1 = new Profile(adr1);
+        Profile prof2 = new Profile(adr2);
+        Profile prof3 = new Profile(adr3);
+
+        List<Profile> profList = List.of(prof1, prof2, prof3);
+        List<Address> expected = List.of(adr1, adr2, adr3);
+
+        Profiles profiles = new Profiles();
+        List<Address> rsl = profiles.collect(profList);
+
+        assertThat(rsl, is(expected));
+    }
+}
