e1fb60f5bd8955b4d2f3e2925fe28a272dd811d3
PK rabota <klimenko161@gmail.com>
2021-12-06 12:42:53 +0300

    1. Метод .of [#70623]

--- src/main/java/ru/job4j/collection/PhoneDictionary.java
+++ src/main/java/ru/job4j/collection/PhoneDictionary.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private ArrayList<Person> persons = new ArrayList<Person>();
+    private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
     }
 
     public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<>();
+        var result = new ArrayList<Person>();
         for (Person person : persons) {
             if (person.getName().equals(key)
                     || person.getSurname().equals(key)
@@ -30,7 +30,7 @@ public class PhoneDictionary {
         Predicate<Person> predAddress = p -> p.getAddress().contains(key);
 
         Predicate<Person> combine = predName.or(predSurname).or(predPhone).or(predAddress);
-        ArrayList<Person> result = new ArrayList<>();
+        var result = new ArrayList<Person>();
         for (Person person : persons) {
             if (combine.test(person)) {
                 result.add(person);
--- src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
@@ -9,61 +9,61 @@ import static org.junit.Assert.assertThat;
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 
     @Test
     public void whenFindByPhone() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         phones.add(
                 new Person("Alexey", "Klimenko", "534872", "Shakhty")
         );
-        ArrayList<Person> persons = phones.find("534872");
+        var persons = phones.find("534872");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
         assertThat(persons.get(1).getSurname(), is("Klimenko"));
     }
 
     @Test
     public void whenFoundNone() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         phones.add(
                 new Person("Alexey", "Klimenko", "534872", "Shakhty")
         );
-        ArrayList<Person> persons = phones.find("Moscow");
+        var persons = phones.find("Moscow");
         assertThat(persons.size(), is(0));
     }
 
     @Test
     public void whenFindWhithPredicateByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.findWhithPredicate("Petr");
+        var persons = phones.findWhithPredicate("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 
     @Test
     public void whenFindWhithPredicateByPhone() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         phones.add(
                 new Person("Alexey", "Klimenko", "534872", "Shakhty")
         );
-        ArrayList<Person> persons = phones.findWhithPredicate("534872");
+        var persons = phones.findWhithPredicate("534872");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
         assertThat(persons.get(1).getSurname(), is("Klimenko"));
     }
