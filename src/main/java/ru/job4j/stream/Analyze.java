8aac83378a5e8b38a1881e5567cf141f961a95ce
Master <klimenko161@gmail.com>
2021-12-08 00:00:48 +0300

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,30 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Analyze {
+
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(el -> el.getSubjects().stream())
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D);
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return List.of();
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return List.of();
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return null;
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return null;
+    }
+}
